83d27b941958bdb012b818be75d053771c1a9225
Ludovic Lafole <ludovic.lafole@gmail.com>
2018-02-22 17:13:15 +0100

    new tempalte

---
+++ src/scripts/lib.js
@@ -0,0 +1,6 @@
+// Import the app dependencies from npm repo
+import jquery from 'jquery'
+import bootstrap from 'bootstrap'
+
+// Export for declarative use in your application
+export { jquery, bootstrap }
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,7 @@
-const {resolve, extname, join} = require('path')
-const HtmlPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const {readdirSync, statSync} = require('fs')
+const env = process.env.NODE_ENV || 'development'
 
-function readDirR(dir) {
-    return statSync(dir).isDirectory()
-        ? readdirSync(dir).reduce((r, f) => r.concat(readDirR(join(dir, f))), [])
-
-        : dir
+function buildConfig(env) {
+  return require('./config/webpack.' + env + '.js')
 }
 
-const src = resolve(__dirname, 'src')
-const dist = resolve(__dirname, 'dist')
-
-let srcFiles = readDirR(src)
-const pages = srcFiles
-    .filter(f =>
-        f.indexOf('partials') < 0 &&
-        extname(f) === '.html'
-    )
-
-module.exports = {
-    entry  : resolve(src, 'index.js'),
-    output : {
-        path    : dist,
-        filename: 'script.js'
-    },
-    module : {
-        rules: [
-            {
-                test  : /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use : ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use     : "css-loader"
-                })
-            },
-            {
-                test  : /\.html$/,
-                loader: 'html-loader?interpolate'
-            },
-            {
-                test: /\.scss$/,
-                use : ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use     : "css-loader!sass-loader"
-                })
-            },
-            {
-                test   : /\.(jpe?g|svg|png|gif|woff2?|eot|ttf)$/,
-                loader : 'file-loader',
-                options: {
-                    name: '[path][name].[ext]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        ...pages.map(page => new HtmlPlugin({
-            template: page,
-            filename: page.replace(src, '.')
-        })),
-        new ExtractTextPlugin("style.css")
-    ]
-}
+module.exports = buildConfig(env === 'production' ? 'prod' : 'dev')
